28b3ee772d74134c41d7a1d629ca714f2cd1c4fa
Aron Bergman <brgmn@icloud.com>
2020-06-01 10:43:42 +0300

    Removed async - await

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const fetch = require('node-fetch');
 const URL = 'https://jsonplaceholder.typicode.com/users/'
 
-const fetch_retry = async (url, n) => {
-    try {
-        return await fetch(url)
-    } catch (err) {
-        if (n === 1) throw err;
-        return await fetch_retry(url, --n);
-    }
-};
+const fetch_retry = (url, n) => fetch(url).catch(function (error) {
+    if (n === 1) throw error;
+    return fetch_retry(url, --n);
+});
 
 fetch_retry(URL, 5)
     .then(response => response.json())
